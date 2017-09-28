46c9758ef9b7dee5e22532115430284e281d1783
chandzul <dchandzul@gmail.com>
2017-09-28 03:23:59 -0500

    Add origin master repositorie

---
+++ src/app.js
@@ -0,0 +1,6 @@
+// const css = require('./app.scss');
+
+// const css = require('../node_modules/bulma/bulma.sass');
+const css = require('./app.sass');
+
+console.log("Hello from Webpack dev server2");
